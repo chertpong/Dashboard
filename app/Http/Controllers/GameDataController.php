52d44615fa626e74d9c553d3800fcf5f4af74340
Chertpong <chertpong_h@cmu.ac.th>
2015-07-21 23:08:04 +0700

    ~update game data api set default value

--- app/Http/Controllers/GameDataController.php
+++ app/Http/Controllers/GameDataController.php
@@ -16,23 +16,23 @@ class GameDataController extends Controller
     public function create(Request $request){
         $gameData = new GameData();
         //Data from request
-        $gameData->name = $request->get('name');
-        $gameData->numPass = $request->get('numPass');
-        $gameData->numLive = $request->get('numLive');
-        $gameData->allDegreeAngle = $request->get('allDegreeAngle');
-        $gameData->distanceHead = $request->get('distanceHead');
-        $gameData->degreeSed = $request->get('degreeSed');
-        $gameData->averageVelocity = $request->get('averageVelocity');
-        $gameData->tomatoesWasted = $request->get('tomatoesWasted');
-        $gameData->tomatoesAvoided = $request->get('tomatoesAvoided');
-        $gameData->rightAvoid = $request->get('rightAvoid');
-        $gameData->leftAvoid = $request->get('leftAvoid');
-        $gameData->downAvoid = $request->get('downAvoid');
-        $gameData->degreeTiltRight = $request->get('degreeTiltRight');
-        $gameData->degreeTiltLeft = $request->get('degreeTiltLeft');
-        $gameData->distanceHeadMoved = $request->get('distanceHeadMoved');
-        $gameData->averageVelocityHeadMoved = $request->get('averageVelocityHeadMoved');
-        $gameData->hash = $request->get('hash');
+        $gameData->name = $request->get('name','');
+        $gameData->numPass = $request->get('numPass','');
+        $gameData->numLive = $request->get('numLive',0);
+        $gameData->allDegreeAngle = $request->get('allDegreeAngle',0);
+        $gameData->distanceHead = $request->get('distanceHead',0);
+        $gameData->degreeSed = $request->get('degreeSed',0);
+        $gameData->averageVelocity = $request->get('averageVelocity',0);
+        $gameData->tomatoesWasted = $request->get('tomatoesWasted',0);
+        $gameData->tomatoesAvoided = $request->get('tomatoesAvoided',0);
+        $gameData->rightAvoid = $request->get('rightAvoid',0);
+        $gameData->leftAvoid = $request->get('leftAvoid',0);
+        $gameData->downAvoid = $request->get('downAvoid',0);
+        $gameData->degreeTiltRight = $request->get('degreeTiltRight',0);
+        $gameData->degreeTiltLeft = $request->get('degreeTiltLeft',0);
+        $gameData->distanceHeadMoved = $request->get('distanceHeadMoved',0);
+        $gameData->averageVelocityHeadMoved = $request->get('averageVelocityHeadMoved',0);
+        $gameData->hash = $request->get('hash',0);
         //Random data
         $gameData->score = rand(0,1200000);
         $gameData->avoid = (rand(0,1000))/10.0;
